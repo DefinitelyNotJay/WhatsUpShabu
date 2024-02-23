92cfd3f5ddc1db6f61c331c51ce9d581c48bedd9
Tendou26 <jjbrathers3@gmail.com>
2024-02-23 21:58:58 +0700

    add tailwind

---
+++ src/receptionist/ManageTable.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body class="w-screen h-screen flex flex-1">
+    <nav class="flex w-2/12 h-full bg-[#EEE8C8]">
+        
+    </nav>
+    <div class="flex flex-col w-full">
+        <nav class="flex justify-between w-full h-[10%] bg-white py-4 px-4">
+            <h1 class="text-4xl font-semibold">การจัดการโต๊ะ</h1>
+            <div class="flex gap-1 items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
+                <p class="text-2xl">พนักงานต้อนรับ</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
+            </div>
+        </nav>
+
+        <!-- main content -->
+        <div class="flex h-full bg-[#D9D9D9] items-center py-6 px-4 ">
+            <div class="grid gap-x-4 gap-y-6 grid-cols-4 grid-rows-3 w-full h-full py-6 px-4 bg-white rounded-3xl overflow-hidden">
+
+                <div class="rounded-xl bg-[#009179]">
+                    <div class="w-full h-1/6 text-center text-xl bg-[#EEE8C8] rounded-t-xl flex items-center justify-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#6A311D" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-utensils"><path d="M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2"/><path d="M7 2v20"/><path d="M21 15V2v0a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3Zm0 0v7"/></svg>
+                        <p class="text-xl font-semibold text-[#6A311D]">A-01</p>
+                    </div>
+                    wdwd
+                </div>
+                
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
