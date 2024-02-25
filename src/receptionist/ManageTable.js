0d3ba3ee3dc23ab33aaf7e72fb8da165993c532e
Tendou26 <jjbrathers3@gmail.com>
2024-02-25 13:05:11 +0700

    finish starter ui and fixed output.css

--- src/receptionist/ManageTable.js
+++ src/receptionist/ManageTable.js
@@ -1,22 +1,37 @@
-const showDialog = () => {};
 
 const openModalBtns = document.getElementsByClassName("openModal");
 const modal = document.getElementById("modal");
 const closeModalBtn = document.getElementById("closeModal");
-const closeBtn = document.getElementById("closeBtn");
+const closeBtn = document.getElementsByClassName("closeBtn");
+
+const tableNo = document.getElementById("table-no");
+
+const openPaymentsBtns = document.getElementsByClassName("openPayments");
+const payment = document.getElementById("payment");
+const closePaymentBtns = document.getElementsByClassName("closePayment");
 
 [...openModalBtns].forEach((element) =>
-  element.addEventListener("click", () => {
+  element.addEventListener("click", (e) => {
     modal.classList.remove("hidden");
   })
 );
 
-console.log(openModalBtns);
+// console.log(openModalBtns);
+console.log(...closeBtn);
 
 closeModalBtn.addEventListener("click", () => {
   modal.classList.add("hidden");
 });
 
-closeBtn.addEventListener("click", () => {
+[...closeBtn].forEach(element => element.addEventListener("click", () => {
   modal.classList.add("hidden");
-});
+}));
+
+[...openPaymentsBtns].forEach(element => 
+  element.addEventListener("click", () => {
+  payment.classList.remove("hidden");
+}));
+
+[...closePaymentBtns].forEach(element => element.addEventListener("click", ()=>{
+  payment.classList.add("hidden");
+}))
